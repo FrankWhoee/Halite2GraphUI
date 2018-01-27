49d3ecca96971c6e8af0345cc616b08be6ec0788
FrankWhoee <34866813+FrankWhoee@users.noreply.github.com>
2018-01-26 22:48:17 -0800

    Add files via upload

--- Halite2db/src/halite2db/GUI.java
+++ Halite2db/src/halite2db/GUI.java
@@ -48,6 +48,7 @@ public class GUI extends javax.swing.JFrame{
         canvas = new javax.swing.JPanel();
         orderChoice = new java.awt.Choice();
         label6 = new java.awt.Label();
+        Title = new java.awt.Label();
 
         checkbox1.setLabel("checkbox1");
 
@@ -87,6 +88,8 @@ public class GUI extends javax.swing.JFrame{
             }
         });
 
+        fieldUsername.setEditable(false);
+        fieldUsername.setEnabled(false);
         fieldUsername.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 fieldUsernameActionPerformed(evt);
@@ -116,6 +119,9 @@ public class GUI extends javax.swing.JFrame{
 
         label6.setText("Order in");
 
+        Title.setFont(new java.awt.Font("Ubuntu", 1, 36)); // NOI18N
+        Title.setText("Graph");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -125,7 +131,9 @@ public class GUI extends javax.swing.JFrame{
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 861, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(canvas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(canvas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(Title, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -152,12 +160,8 @@ public class GUI extends javax.swing.JFrame{
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(canvas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(2, 2, 2)
@@ -182,12 +186,18 @@ public class GUI extends javax.swing.JFrame{
                         .addComponent(label6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(1, 1, 1)
                         .addComponent(orderChoice, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(26, 26, 26)
                         .addComponent(buttonDraw, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(listUsers, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(listUsers, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Title, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(canvas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(29, Short.MAX_VALUE))
         );
 
@@ -239,12 +249,12 @@ public class GUI extends javax.swing.JFrame{
     }
     
     public void initiateChoices(){
-        xChoice.add("mu");
+       // xChoice.add("mu");
         xChoice.add("game_id");
-        xChoice.add("user_id");
-        xChoice.add("rank");
-        xChoice.add("leaderboard_rank");
-        xChoice.add("sigma");
+       // xChoice.add("user_id");
+        //xChoice.add("rank");
+        //xChoice.add("leaderboard_rank");
+        //xChoice.add("sigma");
         
         yChoice.add("mu");
         yChoice.add("game_id");
@@ -346,6 +356,8 @@ public class GUI extends javax.swing.JFrame{
                 System.out.println(e);
             }
             
+            Title.setText("Graph of " + yChoice.getSelectedItem() + " versus " + xChoice.getSelectedItem());
+            
             for(int i = 0; i < data.size() - 2; i++){
                 double beforeX = (double)xAxis.get(i);
                 double beforeY = (double)yAxis.get(i) ;//* (canvas.getHeight()/(highestY - lowestY));
@@ -422,6 +434,7 @@ public class GUI extends javax.swing.JFrame{
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Label Title;
     private java.awt.Button buttonAddUser;
     private java.awt.Button buttonDraw;
     private java.awt.Button buttonRemoveUser;
